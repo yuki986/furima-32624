20a4dc547b580fed88d523a9202aa10033bf3d09
yuki986 <yuki.kuribayashi8@gmail.com>
2021-02-09 10:43:03 +0900

    バリデーションとアソシエーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_reading, presence: true
+  validates :last_name_reading, presence: true
+  validates :birth_day, presence: true
+
+  has_many :products
+  has_many :orders
 end
