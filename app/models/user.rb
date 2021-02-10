1f5f7e8079b48567ab6e09a71eb753adee03ce0e
yuki986 <yuki.kuribayashi8@gmail.com>
2021-02-10 14:30:02 +0900

    新規登録機能とログインの実装
    This reverts commit 8a52f2a6714954b950bab2b4051ef9ea68846def.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,26 +3,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  with_options presence: true do
-    validates :nickname
-    validates :email
-    validates :encrypted_password, format: {
-      with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "半角英数字混合で入力してください。"
-    }
-    with_options format: {
-      with: /\A[ぁ-んァ-ン一-龥々]/, message: "全角（漢字・ひらがな・カタカナ）で入力してください。"
-    } do
-        validates :first_name
-        validates :last_name 
+   with_options presence: true do
+     validates :nickname
+     validates :email
+     validates :password, format: {
+       with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "半角英数字混合で入力してください。"
+      }
+     with_options format: {
+       with: /\A[ぁ-んァ-ン一-龥々]/, message: "全角（漢字・ひらがな・カタカナ）で入力してください。"
+     } do
+         validates :first_name
+         validates :last_name 
+     end
+     with_options format: {
+       with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力する。"
+     } do
+       validates :first_name_reading
+       validates :last_name_reading
+     end
+     validates :birth_day
     end
-    with_options format: {
-      with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力する。"
-    } do
-      validates :first_name_reading
-      validates :last_name_reading
-    end
-    validates :birth_day
-  end
   
 
   has_many :products
