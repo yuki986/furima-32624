8a52f2a6714954b950bab2b4051ef9ea68846def
yuki986 <yuki.kuribayashi8@gmail.com>
2021-02-10 14:29:09 +0900

    Revert "新規登録機能の実装"
    This reverts commit f216136ba8055d91814dc99d98cecb6b5a7d4dd9.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameter, if: :devise_controller?
-
-  private
-  def configure_permitted_parameter
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_reading, :first_name_reading, :birth_day])
-  end
 end
