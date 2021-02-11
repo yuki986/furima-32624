e23cf7e273b0c5ca56a09f577a3f17551b6aea8a
yuki986 <yuki.kuribayashi8@gmail.com>
2021-02-11 21:32:23 +0900

    password登録修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :encrypted_password, format: {
+  validates :password, format: {
            with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "半角英数字混合で入力してください。"
          }
   with_options presence: true do
