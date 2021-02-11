939bb9ab05de38d62f13e1137e4ab87053fdb504
yuki986 <yuki.kuribayashi8@gmail.com>
2021-02-11 16:38:02 +0900

    ユーザー管理機能における単体テストコードの記入

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname {'kuririn'}
+    # email {Faker::Internet.free_email}
+    email {'000000a@.com'}
+    password {'000000a'}
+    password_confirmation {password}
+    first_name {'栗'}
+    last_name {'悠'}
+    first_name_reading {'クリ'}
+    last_name_reading {'ユウ'}
+    birth_day {'2000-10-13'}
+  end
+end
