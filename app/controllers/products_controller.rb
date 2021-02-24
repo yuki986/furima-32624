c3df85a2cbfb3f5901e4ef909077d6cd111d83e5
yuki986 <yuki.kuribayashi8@gmail.com>
2021-02-24 10:35:35 +0900

    urlに直接/editで編集ページへ飛ばないようにした。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,7 +19,9 @@ class ProductsController < ApplicationController
   end
 
   def edit
-    
+    unless current_user.id == @product.user.id
+      redirect_to products_path
+    end
   end
 
   def update
