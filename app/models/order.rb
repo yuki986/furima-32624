89b72915b2a936d72fc5a307a9ccf75f9023d3e2
yuki986 <yuki.kuribayashi8@gmail.com>
2021-02-26 10:57:42 +0900

    orderテーブルのバリデーションの作成

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 class Order < ApplicationRecord
+  with_options presence: true do
+    validates :user_id
+    validates :products_id
+  end
+
   belongs_to :user
   belongs_to :product
   has_one :adress
